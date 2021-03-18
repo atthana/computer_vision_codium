060f77a892ab6d916910d3a84b4c8feeaf57b8e8
atthana-p <atthana.p@codium.co>
2021-03-19 00:26:39 +0700

    Added function to resize but havn't tested yet.

--- resize_images.py
+++ resize_images.py
@@ -1,2 +1,19 @@
-class ResizeImages:
-    pass
+# https://medium.com/@basit.javed.awan/resizing-multiple-images-and-saving-them-using-opencv-518f385c28d3
+
+
+import cv2
+import glob
+import os
+
+inputFolder = 'Cars'
+os.mkdir('Resized')
+i = 0
+
+for img in glob.glob(inputFolder + '/*.jpg'):
+    image = cv2.imread(img)
+    imgResized = cv2.resize(image, (150, 150))
+    cv2.imwrite('Resized/image%041.jpg' %i, imgResized)
+    i += 1
+    cv2.imshow('image', imgResized)
+    cv2.waitKey(30)
+cv2.destroyAllWindows()
