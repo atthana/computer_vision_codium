6fef7df21305bfa54f91773f967d5daff8c79917
atthana-p <atthana.p@codium.co>
2021-03-21 01:24:58 +0700

    Added dataset of all people in CODIUM.

--- known_faces.py
+++ known_faces.py
@@ -1,5 +1,80 @@
 known_faces = [
 
+    ('Ann', 'resize_raw_photos/ann1.jpg'),
+    ('Ann', 'resize_raw_photos/ann2.jpg'),
+    ('Ann', 'resize_raw_photos/ann3.jpg'),
+
+    ('Aey', 'resize_raw_photos/aey1.jpg'),
+    ('Aey', 'resize_raw_photos/aey2.jpg'),
+    ('Aey', 'resize_raw_photos/aey3.jpg'),
+
+    ('Aomsin', 'resize_raw_photos/aomsin1.jpg'),
+    ('Aomsin', 'resize_raw_photos/aomsin2.jpg'),
+    ('Aomsin', 'resize_raw_photos/aomsin3.jpg'),
+
+    ('Oat', 'resize_raw_photos/aot1.jpg'),
+    ('Oat', 'resize_raw_photos/aot2.jpg'),
+    ('Oat', 'resize_raw_photos/aot3.jpg'),
+
+    ('Aum', 'resize_raw_photos/aum1.jpg'),
+    ('Aum', 'resize_raw_photos/aum2.jpg'),
+
+    ('B', 'resize_raw_photos/b1.jpg'),
+    ('B', 'resize_raw_photos/b2.jpg'),
+    ('B', 'resize_raw_photos/b3.jpg'),
+
+    ('Babe', 'resize_raw_photos/babe1.jpg'),
+    ('Babe', 'resize_raw_photos/babe2.jpg'),
+    ('Babe', 'resize_raw_photos/babe3.jpg'),
+    ('Babe', 'resize_raw_photos/babe4.jpg'),
+
+    ('Balloon', 'resize_raw_photos/balloon1.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon2.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon3.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon4.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon5.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon6.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon7.jpg'),
+    ('Balloon', 'resize_raw_photos/balloon8.jpg'),
+
+    ('Boss', 'resize_raw_photos/boss1.jpg'),
+    ('Boss', 'resize_raw_photos/boss2.jpg'),
+    ('Boss', 'resize_raw_photos/boss3.jpg'),
+    ('Boss', 'resize_raw_photos/boss4.jpg'),
+    ('Boss', 'resize_raw_photos/boss5.jpg'),
+    ('Boss', 'resize_raw_photos/boss6.jpg'),
+
+    ('Champ', 'resize_raw_photos/champ1.jpg'),
+
+    ('Deer', 'resize_raw_photos/deer1.jpg'),
+    ('Deer', 'resize_raw_photos/deer2.jpg'),
+    ('Deer', 'resize_raw_photos/deer3.jpg'),
+    ('Deer', 'resize_raw_photos/deer4.jpg'),
+    ('Deer', 'resize_raw_photos/deer5.jpg'),
+    ('Deer', 'resize_raw_photos/deer6.jpg'),
+    ('Deer', 'resize_raw_photos/deer7.jpg'),
+    ('Deer', 'resize_raw_photos/deer8.jpg'),
+    ('Deer', 'resize_raw_photos/deer9.jpg'),
+    ('Deer', 'resize_raw_photos/deer10.jpg'),
+
+    ('Earth', 'resize_raw_photos/earth1.jpg'),
+    ('Earth', 'resize_raw_photos/earth2.jpg'),
+
+    ('Eric', 'resize_raw_photos/eric1.jpg'),
+    ('Eric', 'resize_raw_photos/eric2.jpg'),
+
+    ('Fai', 'resize_raw_photos/fai1.jpg'),
+    ('Fai', 'resize_raw_photos/fai2.jpg'),
+    ('Fai', 'resize_raw_photos/fai3.jpg'),
+
+    ('Ice', 'resize_raw_photos/ice1.jpg'),
+    ('Ice', 'resize_raw_photos/ice2.jpg'),
+    ('Ice', 'resize_raw_photos/ice3.jpg'),
+    ('Ice', 'resize_raw_photos/ice4.jpg'),
+
+    ('Kai', 'resize_raw_photos/kai1.jpg'),
+    ('Kai', 'resize_raw_photos/kai2.jpg'),
+
     ('Q', 'resize_raw_photos/q.jpg'),
     ('Q', 'resize_raw_photos/q1.jpg'),
     ('Q', 'resize_raw_photos/q2.jpg'),
@@ -24,5 +99,132 @@ known_faces = [
     ('Sookjai', 'resize_raw_photos/por1.jpg'),
     ('Sookjai', 'resize_raw_photos/por2.jpg'),
     ('Sookjai', 'resize_raw_photos/por3.jpg'),
+
+    ('Mo', 'resize_raw_photos/mo1.jpg'),
+    ('Mo', 'resize_raw_photos/mo1.jpg'),
+    ('Mo', 'resize_raw_photos/mo2.jpg'),
+    ('Mo', 'resize_raw_photos/mo3.jpg'),
+    ('Mo', 'resize_raw_photos/mo4.jpg'),
+    ('Mo', 'resize_raw_photos/mo5.jpg'),
+    ('Mo', 'resize_raw_photos/mo6.jpg'),
+    ('Mo', 'resize_raw_photos/mo7.jpg'),
+    ('Mo', 'resize_raw_photos/mo8.jpg'),
+    ('Mo', 'resize_raw_photos/mo9.jpg'),
+    ('Mo', 'resize_raw_photos/mo10.jpg'),
+    ('Mo', 'resize_raw_photos/mo11.jpg'),
+
+    ('Norak', 'resize_raw_photos/norak1.jpg'),
+    ('Norak', 'resize_raw_photos/norak2.jpg'),
+    ('Norak', 'resize_raw_photos/norak3.jpg'),
+    ('Norak', 'resize_raw_photos/norak4.jpg'),
+
+    ('P.Pac', 'resize_raw_photos/pac1.jpg'),
+    ('P.Pac', 'resize_raw_photos/pac2.jpg'),
+
+    ('Will', 'resize_raw_photos/will1.jpg'),
+    ('Will', 'resize_raw_photos/will2.jpg'),
+
+    ('Nong', 'resize_raw_photos/nong1.jpg'),
+
+    ('Nui', 'resize_raw_photos/nui1.jpg'),
+
+    ('Riw', 'resize_raw_photos/riw1.jpg'),
+    ('Riw', 'resize_raw_photos/riw2.jpg'),
+
+    ('Ten', 'resize_raw_photos/ten1.jpg'),
+
+    ('Pat', 'resize_raw_photos/pat1.jpg'),
+    ('Pat', 'resize_raw_photos/pat1.jpg'),
+
+    ('Ice-Pani', 'resize_raw_photos/pani1.jpg'),
+    ('Ice-Pani', 'resize_raw_photos/pani2.jpg'),
+    ('Ice-Pani', 'resize_raw_photos/pani3.jpg'),
+
+    ('Kwang', 'resize_raw_photos/kwang1.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang2.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang3.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang4.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang5.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang6.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang7.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang8.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang9.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang10.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang11.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang12.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang13.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang14.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang15.jpg'),
+    ('Kwang', 'resize_raw_photos/kwang16.jpg'),
+
+    ('Meme', 'resize_raw_photos/meme1.jpg'),
+    ('Meme', 'resize_raw_photos/meme2.jpg'),
+    ('Meme', 'resize_raw_photos/meme3.jpg'),
+    ('Meme', 'resize_raw_photos/meme4.jpg'),
+    ('Meme', 'resize_raw_photos/meme5.jpg'),
+    ('Meme', 'resize_raw_photos/meme6.jpg'),
+
+    ('Minnie', 'resize_raw_photos/minnie1.jpg'),
+    ('Minnie', 'resize_raw_photos/minnie2.jpg'),
+
+    ('Mos', 'resize_raw_photos/mos1.jpg'),
+    ('Mos', 'resize_raw_photos/mos2.jpg'),
+    ('Mos', 'resize_raw_photos/mos3.jpg'),
+    ('Mos', 'resize_raw_photos/mos4.jpg'),
+    ('Mos', 'resize_raw_photos/mos5.jpg'),
+    ('Mos', 'resize_raw_photos/mos6.jpg'),
+
+    ('Namo', 'resize_raw_photos/namo1.jpg'),
+    ('Namo', 'resize_raw_photos/namo2.jpg'),
+
+    ('Noongning', 'resize_raw_photos/noongning1.jpg'),
+
+    ('Ri', 'resize_raw_photos/ri1.jpg'),
+    ('Ri', 'resize_raw_photos/ri2.jpg'),
+    ('Ri', 'resize_raw_photos/ri3.jpg'),
+    ('Ri', 'resize_raw_photos/ri4.jpg'),
+
+    ('Toon', 'resize_raw_photos/toon1.jpg'),
+    ('Toon', 'resize_raw_photos/toon2.jpg'),
+    ('Toon', 'resize_raw_photos/toon3.jpg'),
+    ('Toon', 'resize_raw_photos/toon4.jpg'),
+
+    ('Toto', 'resize_raw_photos/toto1.jpg'),
+    ('Toto', 'resize_raw_photos/toto2.jpg'),
+
+    ('Bonus', 'resize_raw_photos/bonus1.jpg'),
+    ('Bonus', 'resize_raw_photos/bonus2.jpg'),
+    ('Bonus', 'resize_raw_photos/bonus3.jpg'),
+
+    ('Best', 'resize_raw_photos/best1.jpg'),
+    ('Best', 'resize_raw_photos/best2.jpg'),
+    ('Best', 'resize_raw_photos/best3.jpg'),
+    ('Best', 'resize_raw_photos/best4.jpg'),
+    ('Best', 'resize_raw_photos/best5.jpg'),
+    ('Best', 'resize_raw_photos/best6.jpg'),
+
+    ('Basit', 'resize_raw_photos/basit1.jpg'),
+    ('Basit', 'resize_raw_photos/basit2.jpg'),
+    ('Basit', 'resize_raw_photos/basit3.jpg'),
+    ('Basit', 'resize_raw_photos/basit4.jpg'),
+    ('Basit', 'resize_raw_photos/basit5.jpg'),
+    ('Basit', 'resize_raw_photos/basit6.jpg'),
+    ('Basit', 'resize_raw_photos/basit7.jpg'),
+    ('Basit', 'resize_raw_photos/basit8.jpg'),
+    ('Basit', 'resize_raw_photos/basit9.jpg'),
+    ('Basit', 'resize_raw_photos/basit10.jpg'),
+    ('Basit', 'resize_raw_photos/basit11.jpg'),
+    ('Basit', 'resize_raw_photos/basit12.jpg'),
+    ('Basit', 'resize_raw_photos/basit13.jpg'),
+    ('Basit', 'resize_raw_photos/basit14.jpg'),
+    ('Basit', 'resize_raw_photos/basit15.jpg'),
+
+    ('Bell', 'resize_raw_photos/bell1.jpg'),
+    ('Bell', 'resize_raw_photos/bell2.jpg'),
+    ('Bell', 'resize_raw_photos/bell3.jpg'),
+
+    ('Boom', 'resize_raw_photos/boom1.jpg'),
+    ('Boom', 'resize_raw_photos/boom2.jpg'),
+    ('Boom', 'resize_raw_photos/boom3.jpg'),
 
 ]
