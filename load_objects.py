f163584204b829b721e18b94d871c1888b3e50a4
atthana-p <atthana.p@codium.co>
2021-03-24 23:39:46 +0700

    Just a bit added comment.

--- load_objects.py
+++ load_objects.py
@@ -1,20 +1,21 @@
 # https://github.com/ageitgey/face_recognition/wiki/How-do-I-save-face-encodings-to-a-file%3F
 
-import face_recognition
 import pickle
+
+import face_recognition
 
 all_face_encodings = {}
 
+print('Start doing dataset')
 img1 = face_recognition.load_image_file("obama.jpeg")
 all_face_encodings["obama"] = face_recognition.face_encodings(img1)[0]
 
 img2 = face_recognition.load_image_file("biden.jpeg")
 all_face_encodings["biden"] = face_recognition.face_encodings(img2)[0]
 
-# ... etc ...
-
 with open('dataset_faces.dat', 'wb') as f:
     pickle.dump(all_face_encodings, f)
 
+print('Finished creating dataset')
 
 # หลังจากโหลดเสร็จจะได้ dataset_faces.dat ขึ้นมานะ
